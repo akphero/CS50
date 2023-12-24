f8e5f1eec7d8e32b445a922c4e9396a08d67c4ab
akphero <alex@alphabusinesscomputers.com>
2023-12-23 19:10:32 -0500

    # Learning loops in C language and how to use them

---
+++ learning-language-C/meow.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// forever loop
+int main(void)
+{
+    while (true)
+    {
+        printf("meow\n");
+    }
+}
+
+
+// forever loop verion #2
+// int main(1)
+// {
+//     while (true)
+//     {
+//         printf("meow\n");
+//     }
+// }
+
+
+// int main(void)
+// {
+//     for (int i = 0; i < 3; i++)
+//     {
+//         printf("meow\n");
+//     }
+// }
+
+// {
+//     int i = 0;
+//     while (i < 3)
+//     {
+//         printf("meow\n");
+//         i++;
+//     }
+
+// }
